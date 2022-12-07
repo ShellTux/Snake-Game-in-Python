444a12d78e2a8d96ac5e8335d22de83a9b86f5c6
ShellTux <luis.pedro.gois00@gmail.com>
2022-12-07 15:58:11 +0000

    Grid setup
    Completed grid setup. Added Grid class with methods, show and update to draw grid on to the screen

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from tkinter import Tk, Canvas
-from random import randint
 
 ROWS = COLS = 30
 WIDTH = HEIGHT = 600
@@ -14,19 +13,39 @@ class App:
         self.window = window
         canvas = Canvas(window, width = width, height = height, bg = background_color)
         canvas.pack()
+        self.canvas = canvas
         self.width = width
         self.height = height
+        self.isRunning = True
 
 class Grid:
     def __init__(self, rows: int, cols: int):
         self.rows = rows
         self.cols = cols
-        self.snake = [
-                (randint(0, rows - 1), randint(0, cols - 1))
-                ]
+
+    def show(self, canvas: Canvas):
+        width = canvas.winfo_width()
+        height = canvas.winfo_height()
+        dw = width / self.cols
+        dh = height / self.rows
+                
+        # Showing lines
+        for i in range(self.rows):
+            canvas.create_line(0, dh * i, width, dh * i, fill = 'white')
+
+        # Showing columns
+        for j in range(self.cols):
+            canvas.create_line(dw * j, 0, dw * j, height, fill = 'white')
+
+    def update(self, canvas: Canvas):
+        self.show(canvas)
+        canvas.update()
 
 
 if __name__ == '__main__':
     myApp = App(WINDOW_TITLE, WIDTH, HEIGHT, background_color = BACKGROUND_COLOR)
+    game_grid = Grid(ROWS, COLS)
 
-    myApp.window.mainloop()
+    while myApp.isRunning:
+        game_grid.update(myApp.canvas)
+    # myApp.window.mainloop()
