02403c8b6880b28614cc8072e56e6ce33443944c
ShellTux <luis.pedro.gois00@gmail.com>
2022-12-07 20:22:02 +0000

    Update

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
+#!/usr/bin/env python3
 from tkinter import Tk, Canvas
 from time import sleep
-from functools import partial
+from random import randint
 
 ROWS = COLS = 30
 WIDTH = HEIGHT = 600
 WINDOW_TITLE: str = 'Snake Game'
 BACKGROUND_COLOR: str = 'black'
+
+class Vector:
+    def __init__(self, x: int | float, y: int | float):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other_vector):
+        return Vector(self.x + other_vector.x, self.y + other_vector.y)
+
+    def __repr__(self):
+        return str((self.x, self.y))
+
+    def __eq__(self, other_vector):
+        if other_vector == None:
+            return False
+        return self.x == other_vector.x and self.y == other_vector.y
+
+    def __iter__(self):
+        return iter((self.x, self.y))
+
+    def copy(self):
+        return Vector(self.x, self.y)
+
 
 class App:
     def __init__(self, title: str, width: int, height: int, *, background_color: str = BACKGROUND_COLOR):
@@ -19,6 +43,53 @@ class App:
         self.width = width
         self.height = height
         self.isRunning = True
+
+
+class Snake:
+    def __init__(self, window: Tk, row: int, col: int, *, head_color: str = 'red', body_color: str = 'green'):
+        self.body = [Vector(row, col)]
+        self.head_color = head_color
+        self.body_color = body_color
+        self.velocity = None
+        window.bind('<Key>', self.change_direction)
+
+    def change_direction(self, key_event):
+        map_direction_to_velocity = {
+                # Up
+                'w': Vector(-1, 0),
+                # Left
+                'a': Vector(0, -1),
+                # Down
+                's': Vector(1, 0),
+                # Right
+                'd': Vector(0, 1)
+                }
+
+        # If dictionary doesn't have key for the key press then exit function
+        if map_direction_to_velocity.get(key_event.char) == None:
+            return
+
+        new_velocity = map_direction_to_velocity[key_event.char]
+        
+        # First check if velocity is not None and
+        # If current velocity and new velocity go in different directions
+        # Then when we multiply each component it will give -1, so we will
+        # Skip self.velocity update because both velocities go in opposite directions, which is not valid
+        if self.velocity != None and (
+                self.velocity.x * new_velocity.x == -1 or
+                self.velocity.y * new_velocity.y == -1
+                ):
+            return
+
+        self.velocity = new_velocity
+
+    def move(self):
+        if self.velocity == None:
+            return
+
+        # Move snake head by self.velocity
+        self.body[0] += self.velocity 
+
 
 class Grid:
     def __init__(self, window: Tk, rows: int, cols: int):
@@ -69,79 +140,44 @@ class Grid:
                 outline = 'white'
                 )
 
+    def check_collision(self):
+        # Check if snake is eating the food
+        print(self.snake.body[0] == self.food)
+        print(self.snake.body[0], self.food)
+        if self.snake.body[0] == self.food:
+            self.generate_food()
+
+
+        # Check if snake body collided with walls
+        if (
+                self.snake.body[0].x < 0 or 
+                self.snake.body[0].y > self.rows or 
+                self.snake.body[0].y < 0 or 
+                self.snake.body[0].y > self.cols
+                ):
+            return True
+
+        # Check if snake collided with it's own body
+        for segment in self.snake.body[1:]:
+            # Collision happened
+            if self.snake.body[0] == segment:
+                return True
+
+        return False
+
+    def generate_food(self):
+        self.food = Vector(randint(0, self.rows - 1), randint(0, self.cols - 1))
 
     def update(self, canvas: Canvas):
         canvas.delete('all')
-        self.show(canvas)
         self.snake.move()
+        if (self.check_collision()):
+            print('collision')
+            # self.window.destroy()
+        self.show(canvas)
         canvas.update()
 
 
-class Vector:
-    def __init__(self, x: int | float, y: int | float):
-        self.x = x
-        self.y = y
-
-    def __add__(self, other_vector):
-        return Vector(self.x + other_vector.x, self.y + other_vector.y)
-
-    def __repr__(self):
-        return str((self.x, self.y))
-
-    def __eq__(self, other_vector):
-        if other_vector == None:
-            return False
-        return self.x == other_vector.x and self.y == other_vector.y
-
-    def __iter__(self):
-        return iter((self.x, self.y))
-
-
-class Snake:
-    def __init__(self, window: Tk, row: int, col: int, *, head_color: str = 'red', body_color: str = 'green'):
-        self.body = [Vector(row, col)]
-        self.head_color = head_color
-        self.body_color = body_color
-        self.velocity = None
-        window.bind('<Key>', self.change_direction)
-
-    def change_direction(self, key_event):
-        map_direction_to_velocity = {
-                # Up
-                'w': Vector(-1, 0),
-                # Left
-                'a': Vector(0, -1),
-                # Down
-                's': Vector(1, 0),
-                # Right
-                'd': Vector(0, 1)
-                }
-
-        # If dictionary doesn't have key for the key press then exit function
-        if map_direction_to_velocity.get(key_event.char) == None:
-            return
-
-        new_velocity = map_direction_to_velocity[key_event.char]
-        
-        # First check if velocity is not None and
-        # If current velocity and new velocity go in different directions
-        # Then when we multiply each component it will give -1, so we will
-        # Skip self.velocity update because both velocities go in opposite directions, which is not valid
-        if self.velocity != None and (
-                self.velocity.x * new_velocity.x == -1 or
-                self.velocity.y * new_velocity.y == -1
-                ):
-            return
-
-        self.velocity = new_velocity
-        print(self.velocity)
-
-    def move(self):
-        if self.velocity == None:
-            return
-
-        # Move snake head by self.velocity
-        self.body[0] += self.velocity 
 
 
 if __name__ == '__main__':
