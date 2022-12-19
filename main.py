2f448152b28f31e104409f8f8c73efd9ee57b7d0
ShellTux <luis.pedro.gois00@gmail.com>
2022-12-19 18:56:49 +0000

    Removed wait_for_quit, refactored code

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from time import sleep
 from Grid import Grid
 from Robot import Robot, strategies
 
-ROWS = COLS = 30
-WIDTH = 600
+ROWS = COLS = 10
+WIDTH = 800
 WINDOW_TITLE: str = 'Snake Game'
 BACKGROUND_COLOR: str = 'black'
 HIGHSCORE_FILE_PATH: str = 'highscore.txt'
@@ -22,22 +22,14 @@ class App:
         window.resizable(False, False)
         self.window = window
         # Label Setup
-        highscore_label = Label(window, text = f'Score: 0 | Highscore: 0', font = ('Helvetica 30 bold'))
+        highscore_label = Label(window, text = f'Score: 0 | Highscore: 0', font = font(size = 30))
         self.highscore_label = highscore_label
         # Canvas setup
         canvas = Canvas(window, width = canvas_width, height = canvas_width, bg = background_color)
         self.canvas = canvas
-        # Width and height of the canvas
-        self.width = self.height = canvas_width
-
-        self.fps: int = fps
-        self.highscore_file_path: str = highscore_file_path
-        self.is_robot_playing: bool = True
-
         # Steps label setup
         steps_label: Label = Label(window, text = 'Movements: 0', font = font())
         self.steps_label = steps_label
-
         # Check button setup
         cpu_check_button: Checkbutton = Checkbutton(
                 window,
@@ -45,16 +37,21 @@ class App:
                 font = font(size = 15),
                 onvalue = True,
                 offvalue = False,
-                state = 'disabled'
+                state = 'normal'
                 )
-
         # Turn on the state of check button
         cpu_check_button.select()
         self.cpu_check_button: Checkbutton = cpu_check_button
-
-
         # Robot Strategies option menu setup
         strategies_menu: OptionMenu = OptionMenu(window, StringVar(window), *strategies)
+
+        # Width and height of the canvas
+        self.width = self.height = canvas_width
+        self.fps: int = fps
+        self.highscore_file_path: str = highscore_file_path
+        self.is_robot_playing: bool = True
+        self.choice: str = ''
+
 
         # Packing all elements into the window
         # Order is crucial
@@ -144,18 +141,18 @@ class App:
                 file.write(f'{self.grid.highscore}\n')
 
     def main_loop(self):
-        # Main menu
-        self.main_menu()
+        while self.main_menu() != 'Quit':
 
-        # Start playing
-        while self.update():
-            sleep(1 / self.fps)
+            # Start playing
+            # Disable cpu check button
+            self.cpu_check_button.config(state = 'disabled')
+            while self.update():
+                sleep(1 / self.fps)
 
-        # Lost
-        print('You Lose!!!')
-        self.cpu_check_button.config(state = 'normal')
-        myApp.save_highscore()
-        myApp.wait_for_quit()
+            # Lost
+            print('You Lose!!!')
+            self.save_highscore()
+            # myApp.wait_for_quit()
 
     def main_menu(self):
         menu_entries = (
@@ -197,18 +194,14 @@ class App:
             elif choice == 'Quit':
                 exit()
 
+            self.choice = choice
+
         self.canvas.bind('<ButtonRelease>', lambda event: press_menu(event))
         self.canvas.bind('<Motion>', move_cursor)
         self.canvas.mainloop()
         self.canvas.unbind('<ButtonRelease>')
         self.canvas.unbind('<Motion>')
-
-    def wait_for_quit(self):
-        # Add a mouse release event to the canvas element to close the window
-        self.canvas.bind('<ButtonRelease>', lambda _: self.window.destroy())
-        self.window.bind('q', lambda _: self.window.destroy())
-
-        self.window.mainloop()
+        return self.choice
 
 
 if __name__ == '__main__':
